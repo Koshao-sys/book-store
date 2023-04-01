1d2f62bb058ebc4207220a245909349470ae2774
Koshao-sys <92449654+Koshao-sys@users.noreply.github.com>
2023-04-01 11:47:07 +0300

    Add Routes and create components

---
+++ src/components/Books.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Books = () => {
+  return (
+    <div>We are getting started...</div>
+  );
+}
+
+export default Books;
---
+++ src/components/Nav.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const Nav = () => {
+  return (
+    <nav className="nav-bar">
+      <ul>
+        <li>BookStore CMS</li>
+        <li><Link to="/">BOOKS</Link></li>
+        <li><Link to="/">CATEGORIES</Link></li>
+      </ul>
+    </nav>
+  );
+}
+
+export default Nav;
